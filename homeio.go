bc5f5c09017f330eab8dcf6ffcda9ab2a4bdb7b7
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-03-27 19:16:39 +0100

    simple communication with bachend

--- homeio.go
+++ homeio.go
@@ -1,25 +1,36 @@
 package main
 
 import (
-  //"net/http"
+  "net/http"
   "github.com/gin-gonic/gin"
   "html/template"
+  
+  "net"
+  "fmt"
+  "bufio"
+  //"os"
+  
+  //"encoding/json"
 )
 
 func main() {
   r := gin.Default()
   r.Static("/assets", "./assets")
-  //r.LoadHTMLGlob("templates/*")
-  
-  var layoutTemplate = "templates/layout.tmpl"
-  //r.SetHTMLTemplate(template.Must(template.ParseFiles("layout.tmpl")))
   
   r.GET("/", func(c *gin.Context) {
     obj := gin.H{"title": "Main website"}
-    r.SetHTMLTemplate(template.Must(template.ParseFiles(layoutTemplate, "templates/index.tmpl")))
-    c.HTML(200, "layout", obj)
+    r.SetHTMLTemplate(template.Must(template.ParseFiles("templates/layout.tmpl", "templates/index.tmpl")))
+    c.HTML(http.StatusOK, "layout", obj)
   })
 
+  r.GET("/payload.json", func(c *gin.Context) {
+    conn, _ := net.Dial("tcp", "127.0.0.1:2005")
+    fmt.Fprintf(conn, "measDetails;\n")
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    c.String(http.StatusOK, message)
+  })
+
+  
   // Listen and serve on 0.0.0.0:8080
   r.Run(":8080")
 }
