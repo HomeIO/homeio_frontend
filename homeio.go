b87c2ec790ea83708d71274e21308ba8f448ef8d
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-04-29 15:09:50 +0200

    trends

--- homeio.go
+++ homeio.go
@@ -71,6 +71,12 @@ func getMeasRawForIndexJson(name string, from uint64,to uint64) string {
   return message
 }
 
+func getMeasStatsJson(name string, from uint64,to uint64) string {
+  conn, _ := net.Dial("tcp", "127.0.0.1:2005")
+  fmt.Fprintf(conn, "measStats;" + name + ";" + strconv.FormatUint(from, 10) + ";" + strconv.FormatUint(to, 10) + ";\n")
+  message, _ := bufio.NewReader(conn).ReadString('\n')
+  return message
+}
 
 func getActionIndexJson() string {
   conn, _ := net.Dial("tcp", "127.0.0.1:2005")
@@ -167,6 +173,13 @@ func main() {
     c.String(http.StatusOK, getMeasRawHistoryForTimeJson(measName, from, to, maxSize))
   })
 
+  // meas#stats
+  r.GET("/api/meas/:name/stats/:from/:to/.json", func(c *gin.Context) {
+    measName := c.Params.ByName("name")
+    from, _ := strconv.ParseUint(c.Params.ByName("from"), 10, 64)
+    to, _ := strconv.ParseUint(c.Params.ByName("to"), 10, 64)
+    c.String(http.StatusOK, getMeasStatsJson(measName, from, to))
+  })
 
   // actions#index  
   r.GET("/api/actions.json", func(c *gin.Context) {
